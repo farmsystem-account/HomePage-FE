144b5776eb728bf3317ccf76fe84d4c07e796800
dongmin0204 <nm2205@dgu.ac.kr>
2025-03-03 18:54:20 +0900

    feat: 블로그 API 모델 추가

---
+++ src/models/blog.ts
@@ -0,0 +1,39 @@
+enum ApiErrorMessages {
+    BLOG_NOT_FOUND = "블로그를 찾을 수 없습니다.",
+    BLOG_DUPLICATED = "이미 신청 처리된 블로그입니다.",
+    ALREADY_SUBMITTED = "이미 승인 처리된 블로그입니다.",
+  }
+
+  enum Track {
+    UNION = "UNION",
+    GAMING_VIDEO = "GAMING_VIDEO",
+    AI = "AI",
+    SECURITY_WEB = "SECURITY_WEB",
+    IOT_ROBOTICS = "IOT_ROBOTICS",
+    BIGDATA = "BIGDATA",
+  }
+
+  interface ApiResponse<T = unknown> {
+    status: number;
+    message: ApiErrorMessages | "요청이 성공했습니다." | string;
+    data?: T;
+  }
+  
+  interface ApiRequest {
+    title : string;
+    description : string;
+    track : Track[]; //트랙은 리스트
+    link : string;
+    
+  }
+
+  interface BlogpageResquest {
+    track : string;
+    generation : number;
+    page : number; // 페이징
+  }
+  
+  // blog에 POST 요청, POST 응답, GET요청 
+  export type ApplyPOSTRequest = ApiRequest;
+  export type ApplyGETResponse = BlogpageResquest;
+  export type ApplyPOSTResponse = ApiResponse<{ blogId: number }>;
