c80947c6ed59baefa300f99594d955e22ffc7855
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-03-05 01:12:05 +0900

    fix: 백엔드 응답에 맞게 모델 + 훅 수정

--- src/models/news.ts
+++ src/models/news.ts
@@ -1,23 +1,25 @@
-interface newsData {
+// 기본적인 Api 응답 형태
+// 스웨거를 믿지마!! 응답을 믿어!!
+interface ApiResponse<T = unknown> {
+  status: number;
+  message: string;
+  data?: T
+}
+
+export interface newsData {
   newsId: number;
   title: string;
   content: string;
 };
 
-interface newsErrorResponse {
-  status: number;
-  message: string;
-  data: string;
-}
-
 // /api/news
-export type NewsGETResponse = newsData[];
-export type NewsIdGETResponse = newsData | newsErrorResponse;
+export type NewsGETResponse = ApiResponse<newsData[]>;
+export type NewsIdGETResponse = ApiResponse<newsData>;
 
 // /api/admin/news
 // 추후 관리자 페이지에 사용할 모델들
 export type AdminNewsPOSTRequest = Omit<newsData, "newsId">;
-export type AdminNewsPOSTResponse = newsData | newsErrorResponse;
+export type AdminNewsPOSTResponse = ApiResponse<newsData>;
 export type AdminNewsPUTRequest = Omit<newsData, "newsId">;
-export type AdminNewsPUTResponse = newsData | newsErrorResponse;
-export type AdminNewsIdDELETEResponse = newsData | newsErrorResponse;
+export type AdminNewsPUTResponse = ApiResponse<newsData>;
+export type AdminNewsIdDELETEResponse = ApiResponse<newsData>;
