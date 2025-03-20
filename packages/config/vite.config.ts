6aa38503627619ec1f4f85bf7ee5234faa8d1d2c
Yunseok <watemark1017@gmail.com>
2025-03-20 13:14:24 +0900

    feat: package.json 추가해서 패키지로 인식 되게

--- packages/config/vite.config.ts
+++ packages/config/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+// import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@config": path.resolve(__dirname, "../config"),
-      "@utils": path.resolve(__dirname, "../utils"),
-      "@shared": path.resolve(__dirname, "../shared"),
-    },
-  }
+  // resolve: {
+  //   alias: {
+  //     "@config": path.resolve(__dirname, "../config"),
+  //     "@utils": path.resolve(__dirname, "../utils"),
+  //     "@shared": path.resolve(__dirname, "../shared"),
+  //   },
+  // }
 });
