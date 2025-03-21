6fd0b4dabd16266c6c32e7f8c506a77dc5777b63
dewbeeny <dewbeeny@gmail.com>
2025-03-21 21:24:30 +0900

    chore: 모노레포 공통 환경 변수 및 터보 설정 적용

--- packages/api/config/apiConfig.ts
+++ packages/api/config/apiConfig.ts
@@ -35,22 +35,3 @@ const apiConfig = axios.create({
 });
 
 export default apiConfig;
-// import axios from "axios";
-
-// const getApiBaseUrl = () => {
-//   const baseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:8080";
-//   return baseUrl.endsWith("/") ? `${baseUrl}api/` : `${baseUrl}/api/`;
-// };
-
-// const API_BASE_URL = getApiBaseUrl();
-
-// const apiConfig = axios.create({
-//   baseURL: API_BASE_URL,
-//   timeout: 5000,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   withCredentials: true,
-// });
-
-// export default apiConfig;
