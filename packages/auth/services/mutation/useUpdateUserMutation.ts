13916f689cc4df080086975f4584391e1bc082b1
dewbeeny <dewbeeny@gmail.com>
2025-03-22 22:05:01 +0900

    feat: React Query 쿼리 키 queryKeys.ts로 분리

--- packages/auth/services/mutation/useUpdateUserMutation.ts
+++ packages/auth/services/mutation/useUpdateUserMutation.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useApi } from "../../../api/hooks/usePrivateApi";
 import { useUserStore } from "../../stores/userStore";
 import { User, UpdateUserRequest } from "../../models/user"; 
+import { queryKeys } from "../queryKeys";
 
 export const useUpdateUserMutation = () => {
   const { patch } = useApi();
@@ -16,7 +17,7 @@ export const useUpdateUserMutation = () => {
     },
     onSuccess: (updatedUser) => {
       setUser(updatedUser); 
-      queryClient.invalidateQueries({ queryKey: ["user", "me"] }); 
+        queryClient.invalidateQueries({ queryKey: queryKeys.user.me }); // 쿼리 캐시 무효화는 쿼리 키로 가능하니깐 쿼리 키 정의 꼭 해주세요!
     },
     onError: () => {
       alert("사용자 정보 수정에 실패했습니다.");
