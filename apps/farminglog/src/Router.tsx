2c8db47740a7da0cc2c44ce387791aee4a01fe1f
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-03-22 18:17:07 +0900

    hotfix: 빌드 에러 해결 + 의존성 몇개 루트로 끌어올리기
    - 한번 수정이면 될 것 같아서 데브에 바로 커밋합니다.

--- apps/farminglog/src/Router.tsx
+++ apps/farminglog/src/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/home'; 
 import Auth from './pages/Auth';
