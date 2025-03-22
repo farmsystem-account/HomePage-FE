3f57c59535fa129add728f924c248ff1cd04b501
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-03-22 18:41:59 +0900

    chore: 파밍로그 기본 세팅 추가
    - 폰트 추가 (광양감동, 프리텐다드 베리어블)
    - css 초기화 코드 추가
    - '@'로 src루트부터 접근하는 절대경로

--- apps/farminglog/src/pages/farminglog/view/index.tsx
+++ apps/farminglog/src/pages/farminglog/view/index.tsx
@@ -1,8 +1,8 @@
 const index = () => {
   return (
     <div>
-      <h1>Farming Log View Page</h1>
-      <p>이 페이지는 파밍 로그 뷰를 위한 임시 페이지입니다.</p>
+      <h1 style={{ fontFamily: "Gwangyang Touching" }}>테스트Farming Log View Page</h1>
+      <p style={{fontFamily: "Pretendard Variable"}}>이 페이지는 파밍 로그 뷰를 위한 임시 페이지입니다.</p>
     </div>
   );
 };
--- apps/farminglog/vite.config.ts
+++ apps/farminglog/vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+    ],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
