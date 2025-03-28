b7fe9c79a79f54b7ac69f54ff87a9a46f0c15682
dewbeeny <dewbeeny@gmail.com>
2025-03-29 00:15:53 +0900

    feat: 리다이렉트 이후 학번 사라지는 문제 해결 (로컬 스토리지에 저장하도록)

--- packages/auth/stores/useAuthStore.ts
+++ packages/auth/stores/useAuthStore.ts
@@ -1,13 +1,13 @@
-import { create } from 'zustand';
-import { combine } from 'zustand/middleware';
+import { create } from "zustand";
+import { persist, combine } from "zustand/middleware";
 
 type AuthStep =
-  | 'start'
-  | 'input'
-  | 'check-name'
-  | 'error'
-  | 'not-member'
-  | 'complete';
+  | "start"
+  | "input"
+  | "check-name"
+  | "error"
+  | "not-member"
+  | "complete";
 
 interface AuthState {
   step: AuthStep;
@@ -19,31 +19,39 @@ interface AuthState {
 }
 
 export const useAuthStore = create(
-  combine<AuthState, any>(
+  persist(
+    combine<AuthState, any>(
+      {
+        step: "start",
+        studentId: "",
+        userName: "",
+        errorMessage: null,
+        errorTitle: null,
+        accessToken: undefined,
+      },
+      (set) => ({
+        setStep: (step: AuthStep) => set({ step }),
+        setStudentId: (studentId: string) => set({ studentId }),
+        setUserName: (userName: string) => set({ userName }),
+        setErrorMessage: (msg: string | null) => set({ errorMessage: msg }),
+        setErrorTitle: (title: string | null) => set({ errorTitle: title }),
+        setToken: (token: string) => set({ accessToken: token }),
+        reset: () =>
+          set({
+            step: "start",
+            studentId: "",
+            userName: "",
+            errorMessage: null,
+            errorTitle: null,
+            accessToken: undefined,
+          }),
+      })
+    ),
     {
-      step: 'start',
-      studentId: '',
-      userName: '',
-      errorMessage: null,
-      errorTitle: null,
-      accessToken: undefined,
-    },
-    (set) => ({
-      setStep: (step: AuthStep) => set({ step }),
-      setStudentId: (studentId: string) => set({ studentId }),
-      setUserName: (userName: string) => set({ userName }),
-      setErrorMessage: (msg: string | null) => set({ errorMessage: msg }),
-      setErrorTitle: (title: string | null) => set({ errorTitle: title }),
-      setToken: (token: string) => set({ accessToken: token }),
-      reset: () =>
-        set({
-          step: 'start',
-          studentId: '',
-          userName: '',
-          errorMessage: null,
-          errorTitle: null,
-          accessToken: undefined,
-        }),
-    })
+      name: "auth-storage", 
+      partialize: (state) => ({
+        studentId: state.studentId, 
+      }),
+    }
   )
 );
