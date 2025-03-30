43813ebc3760b1d19520ddee58802c1f95b489a6
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-03-31 01:16:11 +0900

    feat: 쿼리키 사용해서 캐싱한 서버 상태 업데이트
    - 좋아요 클릭 결과 갱신을 위해 서버에서 데이터 재요청을 하지 않고, 캐싱해둔 상태를 업데이트

--- apps/farminglog/src/pages/farminglog/view/Card.tsx
+++ apps/farminglog/src/pages/farminglog/view/Card.tsx
@@ -55,7 +55,6 @@ export default function Card({ data }: CardProps) {
   const { isApp, isMobile, isDesktop } = useMediaQueries();
   const { mutate: toggleLikeMutate } = useToggleLikeMutation();
   const {
-    setIsNeedRefresh,
     setIsEditMode,
     setFarmingLogId,
     setFarminglogTitle,
@@ -67,8 +66,6 @@ export default function Card({ data }: CardProps) {
     setLiked(prev => !prev);
     setClicked(true);
     toggleLikeMutate(data.farmingLogId);
-    // 좋아요 클릭 시 리프레쉬
-    setIsNeedRefresh(true);
     if (liked) {
       setLikeCount(prev => prev - 1);
     } else { 
