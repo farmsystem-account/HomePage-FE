33cd832faa3499de7c9e93cce78d9816297ce53f
dongmin0204 <nm2205@dgu.ac.kr>
2025-04-07 10:52:17 +0900

    fix: 응원시 나오는 풍선 interface변경

--- apps/farminglog/src/pages/ranking/components/CheerBalloon.tsx
+++ apps/farminglog/src/pages/ranking/components/CheerBalloon.tsx
@@ -1,3 +1,4 @@
+// CheerBalloon.tsx
 import * as S from './CheerBalloon.styles';
 import Good from '@/assets/Icons/good.png';
 import Person from '@/assets/Icons/person.png';
@@ -7,21 +8,26 @@ import { motion } from 'framer-motion';
 interface CheerBalloonProps {
   x: number;
   y: number;
+  /** 랭킹 아이템의 userId, userName 추가 */
+  userId: number;
+  userName: string;
+
   isApp: boolean;
   onClose: () => void;
-  onCheerClick: (e: React.MouseEvent<HTMLDivElement>) => void;
+
+  /** onCheerClick 시 (userId, userName)을 전달 */
+  onCheerClick: (userId: number, userName: string) => void;
   onProfileClick: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-// forwardRef 사용!
 const CheerBalloon = forwardRef<HTMLDivElement, CheerBalloonProps>(
-  ({ x, y, isApp, onCheerClick, onProfileClick }, ref) => {
+  ({ x, y, userId, userName, isApp, onCheerClick, onProfileClick }, ref) => {
     return (
       <S.BalloonWrapper
         as={motion.div}
         ref={ref}
         isApp={isApp}
-        isVisible={true} // or provide a dynamic value based on your logic
+        isVisible={true}
         style={{ top: y, left: x }}
         initial={{ scale: 0.8, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
@@ -29,11 +35,27 @@ const CheerBalloon = forwardRef<HTMLDivElement, CheerBalloonProps>(
         transition={{ duration: 0.2 }}
         onMouseDown={(e) => e.stopPropagation()}
       >
-        <S.Option isApp={isApp} onClick={(e) => { e.stopPropagation(); onCheerClick(e); }}>
+        {/* 응원하기 */}
+        <S.Option
+          isApp={isApp}
+          onClick={(e) => {
+            e.stopPropagation();
+            // (userId, userName) 함께 전달
+            onCheerClick(userId, userName);
+          }}
+        >
           <S.IconImg isApp={isApp} src={Good} alt="응원하기 아이콘" />
           <S.Label isApp={isApp}>응원하기</S.Label>
         </S.Option>
-        <S.Option isApp={isApp} onClick={(e) => { e.stopPropagation(); onProfileClick(e); }}>
+
+        {/* 프로필 보기 */}
+        <S.Option
+          isApp={isApp}
+          onClick={(e) => {
+            e.stopPropagation();
+            onProfileClick(e);
+          }}
+        >
           <S.IconImg isApp={isApp} src={Person} alt="프로필 보기 아이콘" />
           <S.Label isApp={isApp}>프로필 보기</S.Label>
         </S.Option>
