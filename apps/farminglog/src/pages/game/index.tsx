7908d71b193afe63dbe29d03078b6282b73ca9cb
dongmin0204 <nm2205@dgu.ac.kr>
2025-09-30 01:17:16 +0900

    feat: 이미지 등록

--- apps/farminglog/src/pages/game/index.tsx
+++ apps/farminglog/src/pages/game/index.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from 'react'; // useState를 import 합니다.
 import { UnityWebGL } from '../../components/UnityWebGL';
-import { GameContainer, GameTitle, StartButton, StartContainer } from './index.styled.ts';
+import useMediaQueries from "@/hooks/useMediaQueries";
+import { GameContainer, StartButton, StartContainer, LandingHero, UpButton, UpButtonImage } from './index.styled.ts';
+
 
 const Game: React.FC = () => {
   const [isGameStarted, setIsGameStarted] = useState(false);
-
+  const [isLanding, setIsLanding] = useState(true);
+  const { isMobile } = useMediaQueries();
+  const landingImage = 'https://farmsystem-bucket.s3.ap-northeast-2.amazonaws.com/game/DetailGameLanding.png';
+  const upButtonImage = 'https://farmsystem-bucket.s3.ap-northeast-2.amazonaws.com/game/UpGameButton.png';
   const handleStartGame = () => {
     setIsGameStarted(true); 
   };
+  const handleScrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   return (
+    <div
+      style={{ backgroundColor: '#46D77C' }}
+    >
     <GameContainer>
-      <GameTitle>🌱 Grow My Farm</GameTitle>
+
       
       {/** isGameStarted 값에 따라 조건부로 렌더링. */}
       {isGameStarted ? (
@@ -27,6 +38,21 @@ const Game: React.FC = () => {
         </StartContainer>
       )}
     </GameContainer>
+
+    {!isMobile && (
+    <LandingHero
+      $bgDesktop={landingImage}
+      $bgMobile={landingImage}
+      onClick={() => setIsLanding(false)}
+    >
+    </LandingHero>
+    )}
+    {( !isMobile && isLanding &&  
+    <UpButton onClick={handleScrollTop}  title="맨 위로">
+      <UpButtonImage src={upButtonImage} alt="Up Button" />
+      </UpButton>)}
+  
+    </div>
   );
 };
 
